860adf6eb848fc8d6aeb49a5cf5d0b5a4b97c849
Mark Bernhofen <markbernhofen@icloud.com>
2025-08-13 17:04:41 +0100

    enable developer mode install

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prisk",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "plotly",
+        "scipy",
+        "geopandas",
+        "matplotlib"
+    ],
+)
